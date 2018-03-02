876ac954c94f50f05cceba233baea43520569bd6
Mayel de Borniol <git@mayel.space>
2018-03-02 15:35:12 +0200

    s

--- src/Controller/Responses.php
+++ src/Controller/Responses.php
@@ -45,16 +45,16 @@ class Responses extends Admin
         }
 
         if (is_numeric($the_response) && $r->question && $r->question->answer_type=='Tag') { // taxonomy tag
-            $tx = $this->get('Taxonomy');
-            $the_tag = $tx->tag_name_with_ancestors($the_response, $seperator=' ≫ ', 3);
 
-            $this->the_response_tag_id = $the_answer_id = $the_response;
+          $this->the_response_tag_id = $the_answer_id = $the_response;
 
-            $the_response = "<a href='/needs?tag_id=$the_response' target='_blank'>$the_tag</a>";
+          $tx = $this->get('Taxonomy');
+          $the_tag = $tx->tag_name_with_ancestors($the_response, $seperator=' ≫ ', 3);
+          if($the_tag) $the_response = "<a href='/needs?tag_id=$the_response' target='_blank'>$the_tag</a>";
 
         }
 
-        if ($this->the_response_tag_id && $r->question && $r->question->question_name=='tag_new_label	') { // new tag
+        if ($this->the_response_tag_id && $r->question && $r->question->question_name=='tag_new_label') { // new tag
             $the_response .= " <a href='/taxonomy/tag/".$this->the_response_tag_id."/new?label=$the_response' class='btn btn-success' target='_blank'>Confirm Add</a>";
             $this->the_response_tag_id = false;
         }
